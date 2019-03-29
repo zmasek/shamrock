2eb1fbc402bbe3f3db3aa82f42c7fd85ac2486f2
Zlatko Masek <thaidaree@gmail.com>
2019-03-29 22:13:17 +0000

    Add logging.

--- shamrock/shamrock.py
+++ shamrock/shamrock.py
@@ -1,6 +1,10 @@
+import logging
 from urllib import parse
 
 import requests
+from requests.adapters import HTTPAdapter
+from requests.exceptions import HTTPError, Timeout, TooManyRedirects
+from requests.packages.urllib3.util.retry import Retry
 from typing import Any, Callable, Dict, Optional, Tuple
 
 ENDPOINTS: Tuple[str, str, str, str, str, str, str] = (
@@ -12,6 +16,7 @@ ENDPOINTS: Tuple[str, str, str, str, str, str, str] = (
     "plants",
     "species",
 )
+logger: logging.Logger = logging.getLogger(__name__)
 
 
 class Shamrock:
@@ -20,6 +25,12 @@ class Shamrock:
         self.headers: Dict[str, str] = {"Authorization": "Bearer {}".format(token)}
         self.page_size: Optional[int] = page_size
         self.result: Optional[requests.Response] = None
+        self.session: requests.Session = requests.Session()
+        retries: Retry = Retry(
+            total=5,
+            backoff_factor=0.1,
+            status_forcelist=[500, 502, 503, 504])
+        self.session.mount('https://', HTTPAdapter(max_retries=retries))
 
     def __getattr__(self, attr: str) -> Callable[[Any, Any], Any]:
         if attr in ENDPOINTS:
@@ -58,11 +69,28 @@ class Shamrock:
 
     def _get_result(self, kwargs: Dict[str, Any]) -> Any:
         if self.result is not None:
-            built_url = self._get_parametrized_url(kwargs)
+            built_url: str = self._get_parametrized_url(kwargs)
             if built_url == self.result.url:
                 return self.result.json()
-        self.result = requests.get(**kwargs)
-        return self.result.json()
+        try:
+            response: requests.Response = self.session.get(**kwargs)
+        except Timeout:
+            logger.error('The request timed out.')
+        except TooManyRedirects:
+            logger.error('The request had too many redirects.')
+
+        try:
+            response.raise_for_status()
+            try:
+                response_json: Any = response.json()
+            except ValueError:
+                logger.error('Invalid JSON in response.')
+            else:
+                self.session.close()
+                self.result = response
+                return response_json
+        except HTTPError as e:
+            logger.error('Unknown exception raised: ', e)
 
     def search(self, q: str, **kwargs: Any) -> Any:
         query_parameters = {"q": q}
@@ -77,23 +105,19 @@ class Shamrock:
     def next(self) -> Any:
         if self.result is not None and "next" in self.result.links:
             kwargs: Dict[str, Any] = self._kwargs(self.result.links["next"]["url"])
-            self.result: requests.Response = requests.get(**kwargs)
-            return self.result.json()
+            return self._get_result(kwargs)
 
     def previous(self) -> Any:
         if self.result is not None and "prev" in self.result.links:
             kwargs: Dict[str, Any] = self._kwargs(self.result.links["prev"]["url"])
-            self.result: requests.Response = requests.get(**kwargs)
-            return self.result.json()
+            return self._get_result(kwargs)
 
     def first(self) -> Any:
         if self.result is not None and "first" in self.result.links:
             kwargs: Dict[str, Any] = self._kwargs(self.result.links["first"]["url"])
-            self.result: requests.Response = requests.get(**kwargs)
-            return self.result.json()
+            return self._get_result(kwargs)
 
     def last(self) -> Any:
         if self.result is not None and "last" in self.result.links:
             kwargs: Dict[str, Any] = self._kwargs(self.result.links["last"]["url"])
-            self.result: requests.Response = requests.get(**kwargs)
-            return self.result.json()
+            return self._get_result(kwargs)

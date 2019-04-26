3a2698c7e2c76e5e76ca2db3beb39052ae710e6f
Zlatko Masek <thaidaree@gmail.com>
2019-04-26 20:46:11 +0100

    Improve setup by adding twine.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
-        "'MIT license': 'License :: OSI Approved :: MIT License'",
+        "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
